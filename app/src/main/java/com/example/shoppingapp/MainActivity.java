e3f528419a6d45b76b763e10a3fbddc757e2ddc6
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-08-16 18:44:21 +0530

    login layout file added

--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.shoppingapp;
 
 import    androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                Intent i = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
